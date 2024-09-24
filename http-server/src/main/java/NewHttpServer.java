a36dbac8c9ffb7b535d6bd75abbd68e84e574e12
TheFatAndFurious <mrguerrilla@gmail.com>
2024-09-24 22:43:41 +0200

    Can now choose how many servers we start

--- http-server/src/main/java/NewHttpServer.java
+++ http-server/src/main/java/NewHttpServer.java
@@ -5,27 +5,42 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 import java.util.concurrent.Executor;
 
 public class NewHttpServer {
 
+
     public static void main(String[] var0) throws IOException {
-        HttpServer var1 = HttpServer.create(new InetSocketAddress(8080), 0);
-        var1.createContext("/", new TestHandler());
-        var1.setExecutor((Executor)null);
-        System.out.println("Server started on port 8080");
-        var1.start();
-    }
 
-    static class TestHandler implements HttpHandler {
-        TestHandler() {
+        final int ORIGINAL_PORT = 8080;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many servers should we start fam ?");
+        int numberOfServers = scanner.nextInt();
+
+        for (int i = 0; i < numberOfServers; i++) {
+            var port = ORIGINAL_PORT + i;
+            HttpServer var1 = HttpServer.create(new InetSocketAddress(port), 0);
+            var1.createContext("/", new TestHandler(i));
+            var1.setExecutor((Executor) null);
+            System.out.println("Server started on port:" + port);
+            var1.start();
         }
 
-        public void handle(HttpExchange var1) throws IOException {
-            String var2 = "We all good";
-            var1.sendResponseHeaders(200, (long)var2.length());
-            OutputStream var3 = var1.getResponseBody();
-            var3.write(var2.getBytes());
-        }
     }
+        static class TestHandler implements HttpHandler {
+
+            private int serverNumber;
+
+            public TestHandler(int serverNumber){
+                this.serverNumber = serverNumber;
+            }
+
+            public void handle(HttpExchange var1) throws IOException {
+                String var2 = "We all good on server " + serverNumber;
+                var1.sendResponseHeaders(200, (long) var2.length());
+                OutputStream var3 = var1.getResponseBody();
+                var3.write(var2.getBytes());
+            }
+        }
 }
