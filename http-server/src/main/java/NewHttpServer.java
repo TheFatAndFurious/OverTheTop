757f858453bd8e14f7ff7defa499ef15319caff8
TheFatAndFurious <mrguerrilla@gmail.com>
2024-09-25 23:59:43 +0200

    Starting servers ok, made sure the input is more than 0

--- http-server/src/main/java/NewHttpServer.java
+++ http-server/src/main/java/NewHttpServer.java
@@ -11,23 +11,63 @@ import java.util.concurrent.Executor;
 public class NewHttpServer {
 
 
+    static int ORIGINAL_PORT = 8080;
+
     public static void main(String[] var0) throws IOException {
 
         Scanner scanner = new Scanner(System.in);
-        new Thread(()-> {
-            while(true){
+        int numberOfServersToBeginWith = 0;
+
+        do {
+            System.out.println("How many servers should we begin with fam ?");
+            numberOfServersToBeginWith = scanner.nextInt();
+                if (numberOfServersToBeginWith <= 0){
+                    System.out.println("You must start at least 1 server, kiddo. Try again");
+                }
+
+        } while (numberOfServersToBeginWith <= 0);
+        StartServer(numberOfServersToBeginWith);
+
+
+        new Thread(() -> {
+            while (true) {
                 System.out.println("enter a command");
                 String command = scanner.nextLine();
                 ProcessCommand(command);
             }
         }).start();
 
-        final int ORIGINAL_PORT = 8080;
-        System.out.println("How many servers should we start fam ?");
-        int numberOfServers = scanner.nextInt();
 
+    }
 
-        for (int i = 0; i < numberOfServers; i++) {
+    static class TestHandler implements HttpHandler {
+
+        private int serverNumber;
+
+        public TestHandler(int serverNumber) {
+            this.serverNumber = serverNumber;
+        }
+
+        public void handle(HttpExchange var1) throws IOException {
+            String var2 = "We all good on server " + serverNumber;
+            var1.sendResponseHeaders(200, (long) var2.length());
+            OutputStream var3 = var1.getResponseBody();
+            var3.write(var2.getBytes());
+        }
+    }
+
+    private static void ProcessCommand(String command) {
+        String message = switch (command) {
+            case "start" -> "let'start";
+            case "kill" -> "let's kill";
+            default -> "we're cooked";
+        };
+        System.out.println(message);
+
+    }
+
+    private static void StartServer(int numberOfServersToStart) throws IOException {
+        for (int i = 0; i < numberOfServersToStart; i++) {
             var port = ORIGINAL_PORT + i;
             HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
             server.createContext("/", new TestHandler(i));
@@ -35,34 +75,6 @@ public class NewHttpServer {
             System.out.println("Server started on port:" + port);
             server.start();
         }
-        System.out.println("what do we do now ?");
 
     }
-        static class TestHandler implements HttpHandler {
-
-            private int serverNumber;
-
-            public TestHandler(int serverNumber){
-                this.serverNumber = serverNumber;
-            }
-
-            public void handle(HttpExchange var1) throws IOException {
-                String var2 = "We all good on server " + serverNumber;
-                var1.sendResponseHeaders(200, (long) var2.length());
-                OutputStream var3 = var1.getResponseBody();
-                var3.write(var2.getBytes());
-            }
-        }
-
-        private static void ProcessCommand(String command) {
-            switch(command) {
-                case  "start":
-                    System.out.println("let's START");
-                    break;
-                case  "kill":
-                    System.out.println("let's kill");
-                    break;
-            }
-
-        }
 }
