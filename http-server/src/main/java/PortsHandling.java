d83ee94c3dbd789af19b2d9e4a6ae5a836d2e2b5
TheFatAndFurious <mrguerrilla@gmail.com>
2024-10-01 00:36:13 +0200

    reservePort method

--- http-server/src/main/java/PortsHandling.java
+++ http-server/src/main/java/PortsHandling.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
 
 public class PortsHandling {
     // ports range
     private final int FIRST_PORT = 8080;
     private final int LAST_PORT = 9999;
     // available ports
-    private ArrayList<Integer> availablePorts = new ArrayList<>();
+    private Queue<Integer> availablePorts;
 
     public PortsHandling(){
         for (int i = FIRST_PORT; i <= LAST_PORT; i++){
             availablePorts.add(i);
         }
     }
+
+    public synchronized int reservePort() throws Exception {
+        Integer port = availablePorts.poll();
+        if (port != null) {
+            return  port;
+        } else {
+            throw new Exception("No ports available");
+        }
+    }
+
+
 }
